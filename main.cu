9c3b5dc73b0d8940ea825a5ae60bf48903fc86c7
BalintCsala <balint.csala@gmail.com>
2020-06-13 17:04:34 +0200

    Made it BOINC compatible

--- main.cu
+++ main.cu
@@ -7,24 +7,15 @@
 #include <mutex>
 #include <chrono>
 #include <string>
+#include <iostream>
 
 #define RANDOM_MULTIPLIER 0x5DEECE66DULL
 #define RANDOM_ADDEND 0xBULL
 #define RANDOM_MASK ((1ULL << 48ULL) - 1ULL)
-
-#define CHUNK_SEED_BOTTOM_4 (CHUNK_SEED & 0xFULL)
-#define CHUNK_SEED_BIT_5 ((CHUNK_SEED >> 4ULL) & 1ULL)
-
-#ifndef FLOOR_LEVEL
-#define FLOOR_LEVEL 63LL
-#endif
+#define WORK_UNIT_SIZE (1ULL << 23ULL)
 
 #ifndef WANTED_CACTUS_HEIGHT
 #define WANTED_CACTUS_HEIGHT 18LL
-#endif
-
-#ifndef WORK_UNIT_SIZE
-#define WORK_UNIT_SIZE (1ULL << 23ULL)
 #endif
 
 #ifndef BLOCK_SIZE
@@ -34,6 +25,13 @@
 #ifndef GPU_COUNT
 #define GPU_COUNT 1ULL
 #endif
+
+#ifndef FLOOR_LEVEL
+#define FLOOR_LEVEL 63LL
+#endif
+
+/*#define CHUNK_SEED_BOTTOM_4 (CHUNK_SEED & 0xFULL)
+#define CHUNK_SEED_BIT_5 ((CHUNK_SEED >> 4ULL) & 1ULL)
 
 #ifndef OFFSET
 #define OFFSET 0
@@ -65,7 +63,7 @@
 
 #ifndef CACTUS_HEIGHT
 #define CACTUS_HEIGHT 12ULL
-#endif
+#endif*/
 
 #ifdef DEBUG
 // Magic values, don't touch
@@ -87,7 +85,7 @@ __device__ int64_t DEBUG_ARR[]{
 #define ASSERT(k, val)
 #endif
 
-inline __device__ int8_t extract(uint32_t heightMap[], int32_t id) {
+inline __device__ uint8_t extract(uint32_t heightMap[], int32_t id) {
     return (heightMap[id / 5] >> ((id % 5) * 6)) & 0b111111U;
 }
 
@@ -125,20 +123,22 @@ namespace java_random {
 
 }
 
-__global__ __launch_bounds__(BLOCK_SIZE, 2) void crack(uint64_t seed_offset, int32_t *num_seeds, uint64_t *seeds) {
+__global__ __launch_bounds__(BLOCK_SIZE, 2)
+void crack(uint64_t seed_offset, int32_t *num_seeds, uint64_t *seeds, uint8_t chunkSeedBottom4Bits, int16_t neighbor1,
+           int16_t neighbor2, int16_t neighbor3, int16_t diagonalIndex, uint8_t chunkSeedBit5, uint8_t cactusHeight) {
 #ifdef DEBUG
     int32_t debug_index = 0;
     uint64_t originalSeed = 77849775653ULL;
 #else
-    uint64_t originalSeed = ((blockIdx.x * blockDim.x + threadIdx.x + seed_offset) << 4ULL) | CHUNK_SEED_BOTTOM_4;
+    uint64_t originalSeed = ((blockIdx.x * blockDim.x + threadIdx.x + seed_offset) << 4ULL) | chunkSeedBottom4Bits;
 #endif
     uint64_t seed = originalSeed;
 
     uint32_t heightMap[205];
 
 #pragma unroll
-    for (int i = 0; i < 205; i++) {
-        heightMap[i] = 0;
+    for (int i = 0; i < 205; i += 2) {
+        *(int64_t *) (heightMap + i) = 0;
     }
 
     int32_t currentHighestPos = 0, posMap;
@@ -162,11 +162,11 @@ __global__ __launch_bounds__(BLOCK_SIZE, 2) void crack(uint64_t seed_offset, int
         ASSERT(debug_index, initialPos);
 
         if (position == -1) {
-            if (initialPos == NEIGHBOR1) {
+            if (initialPos == neighbor1) {
                 position = 0;
-            } else if (initialPos == NEIGHBOR2) {
+            } else if (initialPos == neighbor2) {
                 position = 1;
-            } else if (initialPos == NEIGHBOR3) {
+            } else if (initialPos == neighbor3) {
                 position = 2;
             }
             ASSERT(debug_index, position);
@@ -175,13 +175,13 @@ __global__ __launch_bounds__(BLOCK_SIZE, 2) void crack(uint64_t seed_offset, int
                 uint64_t bit = (originalSeed >> 4ULL) & 1ULL;
                 ASSERT(debug_index, bit);
 
-                if (position != DIAGONAL_INDEX) {
-                    if (bit == CHUNK_SEED_BIT_5) return;
+                if (position != diagonalIndex) {
+                    if (bit == chunkSeedBit5) return;
                 } else {
-                    if (bit != CHUNK_SEED_BIT_5) return;
+                    if (bit != chunkSeedBit5) return;
                 }
 
-                increase(heightMap, initialPos, CACTUS_HEIGHT);
+                increase(heightMap, initialPos, cactusHeight);
                 ASSERT(debug_index, extract(heightMap, initialPos));
 
                 if (extract(heightMap, currentHighestPos) < extract(heightMap, initialPos)) {
@@ -191,7 +191,8 @@ __global__ __launch_bounds__(BLOCK_SIZE, 2) void crack(uint64_t seed_offset, int
             }
         }
 
-        initialPosY = java_random::next_int_unknown(&seed, ((int32_t)extract(heightMap, initialPos) + FLOOR_LEVEL + 1) * 2);
+        initialPosY = java_random::next_int_unknown(&seed,
+                                                    ((int32_t) extract(heightMap, initialPos) + FLOOR_LEVEL + 1) * 2);
         ASSERT(debug_index, initialPosY);
 
         for (int32_t a = 0; a < 10; a++) {
@@ -205,11 +206,11 @@ __global__ __launch_bounds__(BLOCK_SIZE, 2) void crack(uint64_t seed_offset, int
             ASSERT(debug_index, posMap);
 
             if (position == -1) {
-                if (posMap == NEIGHBOR1) {
+                if (posMap == neighbor1) {
                     position = 0;
-                } else if (posMap == NEIGHBOR2) {
+                } else if (posMap == neighbor2) {
                     position = 1;
-                } else if (posMap == NEIGHBOR3) {
+                } else if (posMap == neighbor3) {
                     position = 2;
                 }
                 ASSERT(debug_index, position);
@@ -218,13 +219,13 @@ __global__ __launch_bounds__(BLOCK_SIZE, 2) void crack(uint64_t seed_offset, int
                     uint64_t bit = (originalSeed >> 4ULL) & 1ULL;
                     ASSERT(debug_index, bit);
 
-                    if (position != DIAGONAL_INDEX) {
-                        if (bit == CHUNK_SEED_BIT_5) return;
+                    if (position != diagonalIndex) {
+                        if (bit == chunkSeedBit5) return;
                     } else {
-                        if (bit != CHUNK_SEED_BIT_5) return;
+                        if (bit != chunkSeedBit5) return;
                     }
 
-                    increase(heightMap, posMap, CACTUS_HEIGHT);
+                    increase(heightMap, posMap, cactusHeight);
                     ASSERT(debug_index, extract(heightMap, posMap));
 
                     if (extract(heightMap, currentHighestPos) < extract(heightMap, posMap)) {
@@ -269,13 +270,16 @@ __global__ __launch_bounds__(BLOCK_SIZE, 2) void crack(uint64_t seed_offset, int
         if (extract(heightMap, currentHighestPos) >= WANTED_CACTUS_HEIGHT) {
             uint64_t neighbor = 0;
             if (position == 0)
-                neighbor = NEIGHBOR1;
+                neighbor = neighbor1;
             if (position == 1)
-                neighbor = NEIGHBOR2;
+                neighbor = neighbor2;
             if (position == 2)
-                neighbor = NEIGHBOR3;
+                neighbor = neighbor3;
             ASSERT(debug_index, neighbor);
-            seeds[atomicAdd(num_seeds, 1)] = (neighbor << 48ULL) | originalSeed;
+            seeds[atomicAdd(num_seeds, 1)] =
+                    ((uint64_t) extract(heightMap, currentHighestPos) << 58ULL) |
+                    ((uint64_t)neighbor << 48ULL) |
+                    originalSeed;
             ASSERT(debug_index, ((neighbor << 48ULL) | originalSeed));
             return;
         }
@@ -294,7 +298,18 @@ void setup_gpu_node(GPU_Node *node, int32_t gpu) {
 }
 
 GPU_Node nodes[GPU_COUNT];
-uint64_t offset = OFFSET;
+uint64_t chunkSeed;
+uint8_t chunkSeedBottom4Bits;
+uint8_t chunkSeedBit5;
+int16_t neighbor1;
+int16_t neighbor2;
+int16_t neighbor3;
+int16_t diagonalIndex;
+uint8_t cactusHeight;
+uint64_t start;
+uint64_t end;
+
+uint64_t offset;
 uint64_t count = 0;
 std::mutex info_lock;
 
@@ -302,18 +317,29 @@ void gpu_manager(int32_t gpu_index) {
     std::string fileName = "kaktoos_seeds" + std::to_string(gpu_index) + ".txt";
     FILE *out_file = fopen(fileName.c_str(), "w");
     cudaSetDevice(gpu_index);
-    while (offset < END) {
+    while (offset < end) {
         *nodes[gpu_index].num_seeds = 0;
-        crack<<<WORK_UNIT_SIZE / BLOCK_SIZE, BLOCK_SIZE, 0>>>(offset, nodes[gpu_index].num_seeds,
-                                                              nodes[gpu_index].seeds);
+        crack<<<WORK_UNIT_SIZE / BLOCK_SIZE, BLOCK_SIZE, 0>>>(
+                offset,
+                nodes[gpu_index].num_seeds,
+                nodes[gpu_index].seeds,
+                chunkSeedBottom4Bits,
+                neighbor1,
+                neighbor2,
+                neighbor3,
+                diagonalIndex,
+                chunkSeedBit5,
+                cactusHeight);
         info_lock.lock();
         offset += WORK_UNIT_SIZE;
         info_lock.unlock();
         cudaDeviceSynchronize();
         for (int32_t i = 0, e = *nodes[gpu_index].num_seeds; i < e; i++) {
 #ifndef DEBUG
-            fprintf(out_file, "%llu %llu\n", nodes[gpu_index].seeds[i] & RANDOM_MASK, (unsigned long long)nodes[gpu_index].seeds[i] >> 48ULL);
-            printf("Found seed: %lld\n", (long long int)nodes[gpu_index].seeds[i]);
+            std::cerr << nodes[gpu_index].seeds[i] << std::endl;
+            printf("Found seed: %llu, height: %llu\n",
+                   nodes[gpu_index].seeds[i] & RANDOM_MASK,
+                   (nodes[gpu_index].seeds[i] >> 58ULL) & 63ULL);
 #endif
         }
         fflush(out_file);
@@ -324,10 +350,40 @@ void gpu_manager(int32_t gpu_index) {
     fclose(out_file);
 }
 
-int main() {
-    printf("Searching %ld total seeds...\n", END - OFFSET);
+int main(int argc, char **argv) {
+    printf("Searching %lld total seeds...\n", (long long) (end - start));
 
     std::thread threads[GPU_COUNT];
+
+    if (argc < 9) {
+        std::cout << "Not enough arguments!" << std::endl;
+
+        return 0;
+    } else {
+        try {
+            start = std::stoull(argv[1]);
+            end = std::stoull(argv[2]);
+            chunkSeed = std::stoull(argv[3]);
+            chunkSeedBottom4Bits = chunkSeed & 15U;
+            chunkSeedBit5 = (chunkSeed >> 4U) & 1U;
+            neighbor1 = std::stoi(argv[4]);
+            neighbor2 = std::stoi(argv[5]);
+            neighbor3 = std::stoi(argv[6]);
+            diagonalIndex = std::stoi(argv[7]);
+            cactusHeight = std::stoi(argv[8]);
+            std::cout << "Starting work on chunk seed " << chunkSeed << std::endl;
+            std::cout <<
+                    "Data: n1: " << neighbor1 <<
+                    ", n2: " << neighbor2 <<
+                    ", n3: " << neighbor3 <<
+                    ", di: " << diagonalIndex <<
+                    ", ch: " << (int)cactusHeight << std::endl;
+        } catch (std::invalid_argument const &ex) {
+            std::cout << "Invalid argument" << std::endl;
+        } catch (std::out_of_range const &ex) {
+            std::cout << "Invalid number size" << std::endl;
+        }
+    }
 
     time_t startTime = time(nullptr), currentTime;
     for (int32_t i = 0; i < GPU_COUNT; i++) {
@@ -337,19 +393,15 @@ int main() {
 
     using namespace std::chrono_literals;
 
-    while (offset < END) {
+    while (offset < end) {
+        std::this_thread::sleep_for(1s);
         time(&currentTime);
         int timeElapsed = (int) (currentTime - startTime);
-        double speed = (double) (offset - OFFSET) / (double) timeElapsed / 1000000.0;
-        printf("Searched %lld seeds, offset: %lld found %lld matches. Time elapsed: %ds. Speed: %.2fm seeds/s. %f%%\n",
-               (long long int) (offset - OFFSET),
-               (long long int) offset,
-               (long long int) count,
-               timeElapsed,
+        double speed = (double) (offset - start) / (double) timeElapsed / 1000000.0;
+        printf("Speed: %.2fm/s, Block: %.5f%%, Block time: %llds\n",
                speed,
-               (double) (offset - OFFSET) / (END - OFFSET) * 100);
-
-        std::this_thread::sleep_for(0.5s);
+               (double)(offset - start) / (double)(end - start) * 100,
+               (long long)timeElapsed);
     }
 
     for (auto &thread : threads) {
